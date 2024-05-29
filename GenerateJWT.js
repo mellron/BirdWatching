b673eaddf4b67718cecc1d76809ce3a2c63f8b74
Doug Tolley <det7777@gmail.com>
2024-05-29 14:49:35 -0400

    Update GenerateJWT.js

--- GenerateJWT.js
+++ GenerateJWT.js
@@ -1,15 +1,32 @@
-// JavaScript Policy - GenerateJWT.js
 var jwt = require('jsonwebtoken');
 
-var userId = context.getVariable('request.formparam.userId');
-var tokenId = context.getVariable('request.formparam.tokenId');
+// Read private key from a secure location (e.g., KVM, environment variable)
+var privateKey = context.getVariable('privateKey'); // Ensure this is securely stored
 
-var privateKey = 'YOUR_PRIVATE_KEY'; // Load your private key securely
+// Get the request body
+var requestBody = context.getVariable('request.content');
+
+// Parse the request body as JSON
+var requestBodyJson = JSON.parse(requestBody);
+
+// Extract userId and tokenId from the request body
+var userId = requestBodyJson.userId;
+var tokenId = requestBodyJson.tokenId;
+
+// Create the JWT payload
 var payload = {
   userId: userId,
   tokenId: tokenId
 };
 
-var token = jwt.sign(payload, privateKey, { algorithm: 'RS256', expiresIn: '1h' });
+// Options for the JWT
+var options = {
+  algorithm: 'RS256', // Use RS256 algorithm
+  expiresIn: '1h'    // Set token expiration time
+};
 
+// Sign the JWT with the private key
+var token = jwt.sign(payload, privateKey, options);
+
+// Set the JWT in the response context
 context.setVariable('jwt', token);
