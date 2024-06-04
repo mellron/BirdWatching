fe1c0aca5edd4a3ab59c5279d9387a006cdb2b45
Doug Tolley <det7777@gmail.com>
2024-06-04 16:14:17 -0400

    Delete OAuthV2 directory

--- OAuthV2/OAuth2TokenServiceConfiguration.cs
+++
@@ -1,6 +0,0 @@
-public class OAuth2TokenServiceConfiguration
-{
-    public string ClientId { get; set; }
-    public string ClientSecret { get; set; }
-    public string TokenEndpoint { get; set; }
-}
